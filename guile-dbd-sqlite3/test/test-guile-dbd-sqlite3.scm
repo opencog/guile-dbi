7ff8e267fb75d36cc18d1830e538c016c7de4e7d
Nala Ginrut <mulei@gnu.org>
2026-05-02 08:14:44 +0800

    Fix wrong expected result for non-existing row

--- guile-dbd-sqlite3/test/test-guile-dbd-sqlite3.scm
+++ guile-dbd-sqlite3/test/test-guile-dbd-sqlite3.scm
@@ -29,33 +29,33 @@
   '(0 . "query ok"))
 
 (check "select"
-  (begin (dbi-query db-obj "select * from testtable where name='testname1'")
-    (dbi-get_status db-obj))
-  '(0 . "query ok"))
+			 (begin (dbi-query db-obj "select * from testtable where name='testname1'")
+							(dbi-get_status db-obj))
+			 '(0 . "query ok"))
 
 (check "get row" (dbi-get_row db-obj) '(("id" . 33) ("name" . "testname1")))
 
 (check "select non-existing row"
-  (begin (dbi-query db-obj "select * from testtable where name='testname'") (dbi-get_status db-obj))
-  '(0 . "query ok"))
+			 (begin (dbi-query db-obj "select * from testtable where name='testname'") (dbi-get_status db-obj))
+			 '(1 . "no more rows available"))
 
 (check "get non-existing row" (dbi-get_row db-obj) #f)
 
 (check "count" (begin (dbi-query db-obj "select count(id) from testtable") (dbi-get_status db-obj))
-  '(0 . "query ok"))
+			 '(0 . "query ok"))
 
 (check "get count" (dbi-get_row db-obj) '(("count(id)" . 3)))
 (dbi-close db-obj)
 
 (check "open/close not leaking file descriptors, not segfaulting"
-  (do ((i 0 (1+ i)) (result #f)) ((> i 2048) result)
-    (let ((db-obj (dbi-open "sqlite3" db-path))) (dbi-query db-obj "SELECT 42")
-      (dbi-query db-obj "BEGIN IMMEDIATE TRANSACTION")
-      (dbi-query db-obj "CREATE TABLE IF NOT EXISTS testtable (id INTEGER)")
-      (dbi-query db-obj "INSERT INTO testtable SET (id) VALUES (23)")
-      (dbi-query db-obj "SELECT * FROM testtable") (dbi-query db-obj "SELECT * FROM testtable")
-      (dbi-query db-obj "SELECT * FROM non-existent") (dbi-query db-obj "COMMIT TRANSACTION")
-      (when (= i 1024) (set! result (dbi-get_status db-obj))) (dbi-close db-obj)))
-  '(0 . "query ok"))
+			 (do ((i 0 (1+ i)) (result #f)) ((> i 2048) result)
+				 (let ((db-obj (dbi-open "sqlite3" db-path))) (dbi-query db-obj "SELECT 42")
+							(dbi-query db-obj "BEGIN IMMEDIATE TRANSACTION")
+							(dbi-query db-obj "CREATE TABLE IF NOT EXISTS testtable (id INTEGER)")
+							(dbi-query db-obj "INSERT INTO testtable SET (id) VALUES (23)")
+							(dbi-query db-obj "SELECT * FROM testtable") (dbi-query db-obj "SELECT * FROM testtable")
+							(dbi-query db-obj "SELECT * FROM non-existent") (dbi-query db-obj "COMMIT TRANSACTION")
+							(when (= i 1024) (set! result (dbi-get_status db-obj))) (dbi-close db-obj)))
+			 '(0 . "query ok"))
 
 (delete-file db-path)
